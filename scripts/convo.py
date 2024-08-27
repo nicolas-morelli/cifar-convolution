f5d1454e59ef884232d2e1f32c8ea728c4e7937d
Nicolás Domingo Morelli <159024539+nicolas-morelli@users.noreply.github.com>
2024-08-27 09:22:31 -0300

    bayesian for hyperband and new hyper ranges

--- scripts/convo.py
+++ scripts/convo.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import tensorflow as tf
 import keras_tuner as kt
@@ -11,20 +12,20 @@ class HyperNet(kt.HyperModel):
         super().__init__()
 
     def build(self, hp):
-        nsteps = hp.Int('nsteps', min_value=1, max_value=5, step=1)
-        nlengthconv = hp.Int('nlengthconv', min_value=1, max_value=5, step=1)
-        kernelsize = hp.Int('kernelsize', min_value=2, max_value=8, step=1)
-        startkernelsize = hp.Int('startkernelsize', min_value=6, max_value=20, step=1)
-        filters = hp.Int('filters', min_value=50, max_value=250, step=2)
-        filterscale = hp.Float('filterscale', min_value=1.0, max_value=4.0, step=0.1)
+        nsteps = hp.Int('nsteps', min_value=1, max_value=4, step=1)
+        nlengthconv = hp.Int('nlengthconv', min_value=1, max_value=4, step=1)
+        kernelsize = hp.Int('kernelsize', min_value=2, max_value=12, step=1)
+        startkernelsize = hp.Int('startkernelsize', min_value=6, max_value=26, step=2)
+        filters = hp.Int('filters', min_value=50, max_value=300, step=5)
+        filterscale = hp.Float('filterscale', min_value=1.0, max_value=6.0, step=0.2)
         padding = hp.Choice('padding', ['valid', 'same'])
         strides = 1
 
-        pool = True  # hp.Boolean('pool')
-        psize = hp.Int('psize', min_value=2, max_value=3)
+        pool = hp.Boolean('pool')
+        psize = hp.Int('psize', min_value=1, max_value=3)
 
-        nlength = hp.Int('nlength', min_value=2, max_value=6, step=1)
-        nwidth = hp.Int('nwidth', min_value=100, max_value=1000, step=5)
+        nlength = hp.Int('nlength', min_value=2, max_value=20, step=2)
+        nwidth = hp.Int('nwidth', min_value=100, max_value=1500, step=5)
 
         input_ = tf.keras.layers.Input(shape=self.ninputs)
 
@@ -66,7 +67,7 @@ class HyperNet(kt.HyperModel):
         output = outputlayer(mid)
 
         model = tf.keras.Model(inputs=[input_], outputs=[output])
-        opt = tf.keras.optimizers.Nadam(learning_rate=hp.Float('learning_rate', min_value=0.0001, max_value=0.01, step=0.00005))
+        opt = tf.keras.optimizers.Nadam(learning_rate=hp.Float('learning_rate', min_value=0.001, max_value=0.2, step=0.001))
 
         model.compile(loss='sparse_categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
 
@@ -75,20 +76,23 @@ class HyperNet(kt.HyperModel):
 
 def main():
 
+    if tf.config.list_physical_devices('GPU'):
+        print("GPU is available.")
+    else:
+        print("GPU is not available.")
+
     (X_train, y_train), (X_test, y_test) = tf.keras.datasets.cifar10.load_data()
 
-    trials = 15
+    trials = 40
 
-    tuner = kt.Hyperband(hypermodel=HyperNet(ninputs=X_train[0].shape, noutputs=len(np.unique(y_train))),
-                         max_epochs=500,
-                         factor=3,
-                         hyperband_iterations=1,
-                         objective='val_accuracy',
-                         overwrite=True,
-                         executions_per_trial=trials,
-                         max_consecutive_failed_trials=trials,
-                         project_name='Convo',
-                         seed=47)
+    tuner = kt.BayesianOptimization(hypermodel=HyperNet(ninputs=X_train[0].shape, noutputs=len(np.unique(y_train))),
+                                    objective='val_accuracy',
+                                    max_trials=trials,
+                                    max_retries_per_trial=0,
+                                    max_consecutive_failed_trials=trials,
+                                    overwrite=True,
+                                    project_name='Convo',
+                                    seed=47)
 
     es = tf.keras.callbacks.EarlyStopping(patience=5, restore_best_weights=True)
 
@@ -97,6 +101,8 @@ def main():
     bestmodel = tuner.get_best_models(num_models=1)[0]
 
     best_hp = tuner.get_best_hyperparameters()[0]
+
+    print(best_hp.values)
 
     bestmodel.save('convmodel.keras')
 
